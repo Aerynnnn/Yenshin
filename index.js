43500077c37f82a4d03d2234e31289d9b99bec0f
Aerynnnn <159232615+Aerynnnn@users.noreply.github.com>
2024-02-26 13:48:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to Yenshin Impact","Watching .gg/yenshin"];
 
 
 let currentIndex = 0;
